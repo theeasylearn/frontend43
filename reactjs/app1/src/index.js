b6e915da85f0f3ec37323e844479d18ca5972ae3
theeasylearn <theeasylearn@gmail.com>
2025-04-09 11:48:07 +0530

    day 3

--- reactjs/app1/src/index.js
+++ reactjs/app1/src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-
+//create virtual dom (required)
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+//create variable to store jsx 
+var output = <><h1 align='center'>Hello World</h1><p>we have just started learning reactjs</p></>;
+root.render(output);
