714d6504ce3e228287ee8cca24d3bf47045ff029
theeasylearn <theeasylearn@gmail.com>
2025-04-11 10:39:58 +0530

    day 5

---
+++ reactjs/app1/src/4_bootstrap_offline_comments.js
@@ -0,0 +1,20 @@
+//offline bootstrap
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+var page = (
+    <div className='container'>
+        {/* this is container */}
+        <div className='row'>
+            {/* this is row */}
+            <div className='col-12'>
+                    <h1 className='border-bottom text-uppercase text-primary'>Working with bootstrap</h1>
+                    <p>We can use bootstrap framework with reactjs using CDN</p>
+            </div>
+        </div>
+    </div>
+)
+root.render(page);
--- reactjs/app1/src/5_function_return_jsx.js
+++ reactjs/app1/src/5_function_return_jsx.js
@@ -6,17 +6,12 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 var time = new Date();
 function showCurrentTime(time)
 {
-    let message = '';
-    //conditional statement
-    if(time.getHours()>11)
-        message = "PM";
-    else 
-        message = "AM";
-
+    //I am function and i can return JSX  too
+    
     var output = (<div className='container'>
         <div className='row'>
             <div className='col-6 offset-3'>
-                <h1 className='text-center'>{time.getHours()} : {time.getMinutes()} : {time.getSeconds()} {message}</h1>
+                <h1 className='text-center'>{time.getHours()} : {time.getMinutes()} : {time.getSeconds()} AM</h1>
             </div>
         </div>
     </div>);
